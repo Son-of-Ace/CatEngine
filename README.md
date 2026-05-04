4d2ee82fecd35896bfd32387941836a9a92889cb
Son-of-Ace <Luka@cedergren.dk>
2026-05-05 01:34:55 +0200

    Added simple triangle render.

---
+++ README.md
@@ -0,0 +1,1 @@
+# HeavenEngine is a custom 2D rendering engine made in C++
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <GL/glew.h>
+#include <string>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Utils/ShaderUtils.hpp"
 
 int main() {
     GLFWwindow *window;
@@ -16,14 +18,74 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        return -1;
+    }
+
+    glViewport(0, 0, 640, 480);
+
+    // Triangle
+    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
+    std::string vertexSource = LoadShader("../shaders/vertex.glsl");
+    std::string fragSource = LoadShader("../shaders/frag.glsl");
+
+    // Vertex Shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    const char *vertexShaderSource = vertexSource.c_str();
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Fragment Shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const char *fragShaderSource = fragSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Buffers
+    unsigned int VBO;
+    unsigned int VAO;
+
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void *>(nullptr));
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwTerminate()x;
+    glfwTerminate();
     return 0;
 }
