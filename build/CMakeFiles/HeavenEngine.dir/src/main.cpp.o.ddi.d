0310df24ff1e67ab7a1b63e197b6428fb3979974
Son-of-Ace <Luka@cedergren.dk>
2026-05-06 16:20:58 +0200

    Remove build dir from git

--- build/CMakeFiles/HeavenEngine.dir/src/main.cpp.o.ddi.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/HeavenEngine.dir/src/main.cpp.o.ddi: \
- /home/luka/Coding/Projects/CPP/HeavenEngine/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/luka/Coding/Projects/CPP/HeavenEngine/src/Application/Application.hpp
